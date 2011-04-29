a35d5d9cdc2cd74a63f085cffc5880c8c308668c
Ted Schmidt <ted@sense-os.nl>
2011-04-29 07:18:35 +0000

    Changed the noise sensor to use rms ldB

--- src/nl/sense_os/service/ambience/NoiseSensor.java
+++ src/nl/sense_os/service/ambience/NoiseSensor.java
@@ -47,20 +47,26 @@ public class NoiseSensor extends PhoneStateListener {
                 }
 
                 byte[] buffer = new byte[BUFFER_SIZE];
+                                
                 int readBytes = 0;
-                if (audioRecord != null) {
+                if (audioRecord != null) {                	
                     readBytes = audioRecord.read(buffer, 0, BUFFER_SIZE);
                 }
-
+                                
                 if (readBytes < 0) {
                     Log.e(TAG, "Error reading AudioRecord buffer: " + readBytes);
                     return -1;
+                }     
+                double ldb = 0;
+                for (int x = 0; x < readBytes-1; x = x+2) {
+                	// it looks like little endian                	
+                	double val = buffer[x+1] << 8 | buffer[x];
+                	ldb += val*val;                	
+                    //dB += Math.abs(buffer[x]);                                    
                 }
-
-                for (int x = 0; x < readBytes; x++) {
-                    dB += Math.abs(buffer[x]);
-                }
-                dB /= (double) readBytes;
+               
+                ldb /= ((double)readBytes/2);                
+                dB = (20.0*Math.log10(Math.sqrt(ldb)));              
 
             } catch (Exception e) {
                 Log.e(TAG, "Exception calculating noise Db!", e);
@@ -304,10 +310,9 @@ public class NoiseSensor extends PhoneStateListener {
     private static final String NAME_NOISE = "noise_sensor";
     private static final String NAME_MIC = "microphone";
     private static final int MAX_FILES = 60;
-    private static final int DEFAULT_SAMPLE_RATE = 8000;
-    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(DEFAULT_SAMPLE_RATE,
-            AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT) * 10;
+    private static final int DEFAULT_SAMPLE_RATE = 8000;               
     private static final int RECORDING_TIME_NOISE = 2000;
+    private static final int BUFFER_SIZE = DEFAULT_SAMPLE_RATE * 2 * 2; //  samples per second * 2 seconds, 2 bytes
     private static final int RECORDING_TIME_STREAM = 60000;
     private AudioRecord audioRecord;
     private boolean isEnabled = false;
@@ -371,11 +376,11 @@ public class NoiseSensor extends PhoneStateListener {
         // create the AudioRecord
         if (isCalling) {
             audioRecord = new AudioRecord(MediaRecorder.AudioSource.VOICE_UPLINK,
-                    DEFAULT_SAMPLE_RATE, AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                    DEFAULT_SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
                     AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
         } else {
             audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, DEFAULT_SAMPLE_RATE,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO, AudioFormat.ENCODING_PCM_16BIT,
+                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
                     BUFFER_SIZE);
         }
 
