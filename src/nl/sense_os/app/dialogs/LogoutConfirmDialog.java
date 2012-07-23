1d181f736ff3abc5ac9314d0a50daf1c6bff4f1d
Steven Mulder <steven@sense-os.nl>
2012-07-23 11:22:31 +0000

    made logout confirm dialog use default constructor

--- src/nl/sense_os/app/dialogs/LogoutConfirmDialog.java
+++ src/nl/sense_os/app/dialogs/LogoutConfirmDialog.java
@@ -12,41 +12,48 @@ import android.support.v4.app.DialogFragment;
 
 public class LogoutConfirmDialog extends DialogFragment {
 
-    public static interface LogoutActivity {
-        void logout();
-    }
+	public static interface LogoutActivity {
+		void logout();
+	}
 
-    private LogoutActivity listener;
+	/**
+	 * @param listener
+	 *            Listener for click events on the dialog
+	 * @return A new LogoutConfirmDialog
+	 */
+	public static LogoutConfirmDialog create(LogoutActivity listener) {
+		LogoutConfirmDialog dialog = new LogoutConfirmDialog();
+		dialog.listener = listener;
+		return dialog;
+	}
 
-    public LogoutConfirmDialog(LogoutActivity listener) {
-        this.listener = listener;
-    }
+	private LogoutActivity listener;
 
-    @TargetApi(11)
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+	@TargetApi(11)
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        Bundle args = getArguments();
-        String username = args.getString("username");
+		Bundle args = getArguments();
+		String username = args.getString("username");
 
-        // create builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            // specifically set dark theme for Android 3.0+
-            builder = new AlertDialog.Builder(getActivity(), AlertDialog.THEME_HOLO_DARK);
-        }
+		// create builder
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// specifically set dark theme for Android 3.0+
+			builder = new AlertDialog.Builder(getActivity(), AlertDialog.THEME_HOLO_DARK);
+		}
 
-        builder.setIcon(android.R.drawable.ic_dialog_alert);
-        builder.setMessage(R.string.dialog_logout_msg);
-        builder.setTitle(getString(R.string.dialog_logout_title, username));
-        builder.setPositiveButton(R.string.button_logout, new OnClickListener() {
+		builder.setIcon(android.R.drawable.ic_dialog_alert);
+		builder.setMessage(R.string.dialog_logout_msg);
+		builder.setTitle(getString(R.string.dialog_logout_title, username));
+		builder.setPositiveButton(R.string.button_logout, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                listener.logout();
-            }
-        });
-        builder.setNegativeButton(android.R.string.cancel, null);
-        return builder.create();
-    }
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				listener.logout();
+			}
+		});
+		builder.setNegativeButton(android.R.string.cancel, null);
+		return builder.create();
+	}
 }
